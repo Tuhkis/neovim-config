b80e1bf4759bc7de433b9edddd2e9d8867c063f9
Tuhkis <nicolindstromj@gmail.com>
2022-12-17 21:01:44 +0200

    Make lsp workie

--- lua/mason-config.lua
+++ lua/mason-config.lua
@@ -1,2 +1,15 @@
-require("mason").setup()
+local mason = require("mason")
 require("mason-lspconfig").setup()
+
+mason.setup()
+
+require("mason-lspconfig").setup_handlers {
+	-- The first entry (without a key) will be the default handler
+        -- and will be called for each installed server that doesn't have
+        -- a dedicated handler.
+        function (server_name) -- default handler (optional)
+            require("lspconfig")[server_name].setup {}
+        end,
+        -- Next, you can provide a dedicated handler for specific servers.
+        -- For example, a handler override for the `rust_analyzer`:
+}
